ec8023c8b2e900630029e77cdf073a17c9d0fa0c
timothychou <tchou@caltech.edu>
2016-02-24 04:13:42 -0800

    debugging

--- create_strats.py
+++ create_strats.py
@@ -2,11 +2,13 @@ import json
 import networkx as nx
 import heapq
 import sys
-
+from tqdm import *
+import os
 filename = '2.5.2'
-nodes = 5
-if len(sys.argv) > 2:
+nodes = 10
+if len(sys.argv) > 1:
     filename = sys.argv[1]
+if len(sys.argv) > 2:
     nodes = int(sys.argv[2])
 print filename
 print nodes
@@ -22,45 +24,80 @@ with open(filename) as json_file:
 bc = nx.betweenness_centrality(G)
 cc = nx.closeness_centrality(G)
 dc = nx.degree_centrality(G)
-
+print "graph analysis completed"
 def gen_strats(res, nodes):
     strats = {}
-    for alpha in range(0, 100 + res, res):
+    for alpha in tqdm(range(0, 100 + res, res)):
         for beta in range(0, 100 + res - alpha, res):
             gamma = 100 - alpha - beta
-            init_scores = []
-            top = []
-            
-            #linear combination of BC, CC, DC
-            for i in range(len(G.nodes())):
-                score = alpha / 0.07 * bc[i] + beta / 0.54 * cc[i] + gamma / 0.28 * dc[i]
-                init_scores.append(score)
-            
-            #pagerank like algo
-            for i in range(len(G.nodes())):
-                new_score = init_scores[i]
-                for nbr in G.neighbors(i):
-                    new_score += init_scores[nbr] / G.degree(nbr)
 
-                if len(top) < nodes:
-                    heapq.heappush(top, (new_score, i))   
-                elif new_score > top[0][0]:
-                    heapq.heappop(top)
-                    heapq.heappush(top, (new_score, i))
+            for pagerank in [True, False]:
+                init_scores = []
+                top = []
+            
+                #linear combination of BC, CC, DC
+                for i in range(len(G.nodes())):
+                    score = alpha / 0.07 * bc[i] + beta / 0.54 * cc[i] + gamma / 0.28 * dc[i]
+                    init_scores.append(score)
+            
+                #pagerank like algo
+                for i in range(len(G.nodes())):
+ 
+                    new_score = init_scores[i]
+                    if pagerank:
+                        for nbr in G.neighbors(i):
+                            new_score += init_scores[nbr] / G.degree(nbr)
+
+                    if len(top) < nodes:
+                        heapq.heappush(top, (new_score, i))   
+                    elif new_score > top[0][0]:
+                        heapq.heappop(top)
+                        heapq.heappush(top, (new_score, i))
    
-            best_nodes = []
-            for score, i in top:
-                best_nodes.append(i)
-            key = '(' + str(alpha) + ',' + str(beta) + ',' + str(gamma) + ')'
+                best_nodes = []
+                for score, i in top:
+                    best_nodes.append(i)
+                key = 'lin comb: (' + str(alpha) + ',' + str(beta) + ',' + str(gamma) + ',' + str(pagerank) + ')'
+                strats[key] = best_nodes
+
+    for alpha in tqdm(range(nodes)):
+        for beta in range(nodes - alpha):
+            gamma = nodes - alpha - beta
+            for pagerank in [True, False]:
+                tops = [[], [], []]
+                for i in range(len(G.nodes())):
+                    newB = bc[i]
+                    newC = cc[i]
+                    newD = dc[i]
+                    if pagerank:
+                        for nbr in G.neighbors(i):
+                            newB += bc[nbr] / G.degree(nbr)
+                            newC += cc[nbr] / G.degree(nbr)
+                            newD += dc[nbr] / G.degree(nbr)
+                    for top, new_score, thresh in zip(tops, [newB, newC, newD], [alpha, beta, gamma]):
+                        if len(top) < thresh:
+                            heapq.heappush(top, (new_score, i))
+                        elif thresh > 0 and new_score > top[0][0]:
+                            heapq.heappop(top)
+                            heapq.heappush(top, (new_score, i))
+                best_nodes = []
+                for top in tops:
+                    for score, i in top:
+                        best_nodes.append(i)
+
+            key = 'mixed: (' + str(alpha) + ',' + str(beta) + ',' + str(gamma) + ',' + str(pagerank) + ')'
             strats[key] = best_nodes
+            
     return strats
                             
 our_strats = gen_strats(5, nodes)
 ta_strats = gen_strats(10, int(nodes * 1.2))
 
-with open("p1.txt", "w") as outfile:
+with open("output/" + filename[:-5] + "/p1.txt", "w") as outfile:
     json.dump(our_strats, outfile)
     
-with open("p2.txt", "w") as outfile:
+with open("output/" + filename[:-5] + "/p2.txt", "w") as outfile:
     json.dump(ta_strats, outfile)
+
+os.system('cp ' + filename + ' output/' + filename[:-5] + '/' + filename)
 
--- runSim.py
+++ runSim.py
@@ -10,22 +10,28 @@ import os
 # The JSON is in the format:
 # numPlayers.numSeeds.uniqueID.json
 from tqdm import *
-dir = 'output'
+dir = 'output/2.5.2'
+if len(sys.argv) > 1:
+    dir = 'output/' + sys.argv[1]
 scores = {}
-for dirName in os.listdir(dir):
+wins = {}
+for dirName in ['']:
     data = {}
     if dirName.startswith('.'):
         continue
     for fname in os.listdir(dir + '/' + dirName):
+        if fname.startswith('.'):
+            continue
         if fname[-5:] == '.json':
-            with open(dir + '/' + dirName + '/' + fname, 'r') as f:
+            with open(dir + '/'  + fname, 'r') as f:
                 adj_list = json.load(f)
         else:
-            with open(dir + '/' + dirName + '/' + fname, 'r') as f:
+            with open(dir + '/'  + fname, 'r') as f:
                 print('loading %s into %s' %(fname, fname[:2]))
                 data[fname[:2]] = json.load(f)
 
     iter = 0
+    print("iterations: " + str(len(data['p1'])))
     for key1, val1 in tqdm(data['p1'].iteritems()):
 
 
@@ -35,22 +41,37 @@ for dirName in os.listdir(dir):
             
             temp = scores.get(key1,{})
             temp[key2] = temp.get(key2, 0) + sim.run(adj_list, d)['p1']
+
             scores[key1] = temp
 
-with open('costMatrix', 'w') as f:
+            temp = wins.get(key1, {})
+            temp[key2] = int(scores[key1][key2] > 250)
+            wins[key1] = temp
+
+            
+with open(dir + '/' + 'costMatrix', 'w') as f:
     json.dump(scores, f)
 
-keys1 = scores.keys()
-keys2 = scores[keys1[0]].keys()
+with open(dir + '/' + 'winMatrix', 'w') as f:
+    json.dump(wins, f)
+
+keys1 = sorted(scores.keys())
+keys2 = sorted(scores[keys1[0]].keys())
 lookUp1 = {keys1[i]: i for i in range(len(keys1))}
-lookUp2 = {keys2[i]: i for i in range(len(kyes2))}
-scoreMatrix = np.zeros(len(keys1), len(keys2))
+lookUp2 = {keys2[i]: i for i in range(len(keys2))}
+scoreMatrix = np.zeros((len(keys1), len(keys2)))
+winMatrix = np.zeros((len(keys1), len(keys2)))
 for key1 in keys1:
     for key2 in keys2:
-        scoreMatrix[lookUp1[keys1]][lookUp2[keys2]] = scores[key1][key2]
+        scoreMatrix[lookUp1[key1]][lookUp2[key2]] = scores[key1][key2]
+        winMatrix[lookUp1[key1]][lookUp2[key2]] = wins[key1][key2]
+        
 
-np.savetxt('costMatrix.csv', scoreMatrix, delimiter=",")
-
+np.savetxt(dir + '/costMatrix.csv', scoreMatrix, delimiter=",")
+np.savetxt(dir + '/winMatrix.csv', winMatrix, delimiter=",")
+with open(dir + '/keys.txt', 'w') as f:
+    f.write(str(keys1))
+    f.write(str(keys2))
 
 
             
