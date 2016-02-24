646ba84ea19a4b6f8e053f764bf15c146e409486
timothychou <tchou@caltech.edu>
2016-02-23 20:27:54 -0800

    Updated visualization and changed sim to simulate based off of strat output. sim now changed to runStrat

--- runSim.py
+++ runSim.py
@@ -2,51 +2,44 @@ import sim
 import sys
 import subprocess
 import json
-
-if len(sys.argv) < 2:
-    print "Usage: python runSim.py graph_filename.json strat1 strat2 ..."
-    sys.exit()
+import os
 
 # file represents the JSON file.
 
 # The JSON is in the format:
 # numPlayers.numSeeds.uniqueID.json
+from tqdm import *
+dir = 'output'
+scores = {}
+for dirName in os.listdir(dir):
+    data = {}
+    if dirName.startswith('.'):
+        continue
+    for fname in os.listdir(dir + '/' + dirName):
+        if fname[-5:] == '.json':
+            with open(dir + '/' + dirName + '/' + fname, 'r') as f:
+                adj_list = json.load(f)
+        else:
+            with open(dir + '/' + dirName + '/' + fname, 'r') as f:
+                print('loading %s into %s' %(fname, fname[:2]))
+                data[fname[:2]] = json.load(f)
 
-file = sys.argv[1]
-info = file.split('.')
+    iter = 0
+    for key1, val1 in tqdm(data['p1'].iteritems()):
 
-num_players = int(info[0])
-num_seeds = int(info[1])
-graph_id = int(info[2])
 
-# Python strategy files.
-strats = sys.argv[2:]
+        for key2, val2 in data['p2'].iteritems():
+            d = {'p1': [str(x) for x in val1], 
+                 'p2': [str(x) for x in val2]}
+            
+            temp = scores.get(key1,{})
+            temp[key2] = temp.get(key2, 0) + sim.run(adj_list, d)['p1']
+            scores[key1] = temp
 
-# This simulation is supposed to simulate people playing against each other.
-# So, we want there to be strategies for each person.
-if num_players != len(strats):
-    print "Usage: python runSim.py graph_filename.json strat1 strat2 ..."
-    sys.exit()
-
-# This simulation is here to simulate people playing against each other.
-d = {}
-for i in range(num_players):
-
-    # Each player will invoke a call to the their particular strategy.
-    subprocess.call(["python", strats[i], sys.argv[1], "1"])
-
-    # The dictionary will then store as a key value:
-    # key = [STRATEGY_NAME][PLAYERID]
-    # where the STRATEGY_NAME is the python file (without the .py), and the
-    # PLAYERID is the integer that corresponds to the particular player.
-
-    # The value stored in this dictionary will be the particular output for this
-    # particular strategy. This output is the adjacency list.
-    d['%s%d' %(str(strats[i][:-3]), i)] = [line.rstrip('\n') for line in open('output.txt')]
+with open('costMatrix', 'w') as f:
+    json.dump(scores, f)
 
 
 
-with open(sys.argv[1]) as data_file:
-    data = json.load(data_file)
 
-print sim.run(data,d)
+            
--- visualize.py
+++ visualize.py
@@ -3,7 +3,7 @@ from networkx.readwrite import json_graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-filename = '2.5.2.json'
+filename = '2.5.2'
 G = nx.Graph()
 with open(filename) as json_file:
     data = json.load(json_file)
@@ -11,11 +11,14 @@ with open(filename) as json_file:
         G.add_node(i)
         for j in range(len(data[str(i)])):
             G.add_edge(i, int(data[str(i)][j]))
-
+dic = {}
+print G.size()
+for i in range(len(G.nodes())):
+    dic[i] = i
 for i in range(1):
     plt.figure()
     plt.title('spring')
-    nx.draw_spring(G, node_size=10,width=.2)
+    nx.draw_spring(G, node_size=0,width=.2, labels=dic, font_size=8, font_color='red')
 
 
 plt.show()
