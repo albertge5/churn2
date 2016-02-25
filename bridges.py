52e8db7cb3e5fcabf3f8699bb6f7ec37365da974
John Li <johnzli1995@gmail.com>
2016-02-24 20:11:27 -0800

    Fixed a bug in my DFS

--- bridges.py
+++ bridges.py
@@ -23,29 +23,27 @@ def createAdjList(edges):
         srcNode = edge[0]
         destNode = edge[1]
 
-        # Since this is undirected, we ensure that the adjacency list 
-        # goes both ways.
+        # Since this is we want this to be directed (for bridge detection)
         if srcNode in adjList:
             adjList[srcNode].append(destNode)
 
         else:
             adjList[srcNode] = [destNode]
 
-        if destNode in adjList:
-            adjList[destNode].append(srcNode)
-
-        else:
-            adjList[destNode] = [srcNode]
-
     return adjList
 
-
+# This will output an adjacencyList to a JSON file. This is just to check
+# the legitimacy of an adjacency list.
+def listToJSON(adjacencyList):
+    jsonString = json.dumps(adjacencyList)
+    outFile = open("adjList", "w")
+    outFile.write(jsonString)
 
 # Like the name suggests, this simply just finds the minimum spanning tree
 # of the graph.
 def findMinSpanTrees(adjacencyList):
     # Since we don't really have any weights associated with each of
-    # the edges, this algorithm is basically BFS. 
+    # the edges, this algorithm is basically DFS. 
     # We take the first node in the adjacency list to be our start.
 
     # Edges will contain tuples, representing edges
@@ -61,8 +59,10 @@ def findMinSpanTrees(adjacencyList):
     # We want the total number of vertices.
     numVertices = len(adjacencyList.keys())
 
-    # Queue for BFS
-    vertexQueue = Queue.Queue()
+    # Stack for DFS
+    stack = []
+
+    stack.append("DONE")
 
     # Start Index in case for disconnected nodes.
     startIdx = 0
@@ -83,22 +83,24 @@ def findMinSpanTrees(adjacencyList):
     # Once we hit a connected component, we start populating DAT QUEUE.
     for neighbor in neighbors:
         edge = (startNode, neighbor)
-        vertexQueue.put(edge)
 
-        vertices.append(neighbor)
-        edges.append(edge)
+        # The moment we can move, we stop the for loop.
+        if neighbor not in vertices:
+            currEdge = edge
+            vertices.append(neighbor)
+            edges.append(edge)
+            break
 
     while len(vertices) != numVertices:
         # Two cases here:
-        # 1: The queue is now empty (we exhausted this component)
+        # 1: The stack is now empty (we exhausted this component)
         #       We first make an adjacency matrix for this.
         #       In this case, we keep incrementing startIdx until
         #       we encounter an unvisited connected component.
         #
-        # 2: The queue is not empty
+        # 2: The stack is not empty
         #       In this case, we just keep going through that component.
-
-        if vertexQueue.empty():
+        if currEdge == "DONE":
             # First, adding the new edges and the adjacency matrix into
             # the appropriate lists.
             treeEdges.append(edges)
@@ -132,29 +134,34 @@ def findMinSpanTrees(adjacencyList):
             for neighbor in neighbors:
                 edge = (startNode, neighbor)
                 if neighbor not in vertices:
-                    vertexQueue.put(edge)
-
+                    stack.append("DONE")
+                    currEdge = edge
                     vertices.append(neighbor)
                     edges.append(edge)
+                    break
 
         # Occurs if there already is stuff inside the Queue.
         else:
-            currEdge = vertexQueue.get()
             srcNode = currEdge[0]
             destNode = currEdge[1]
 
             # Both nodes should technically be inside the array.
             nextNeighbors = adjacencyList[destNode]
-
             # There should always be neighbors in this case.
+            unVisitedNeighbor = False
+
             for neighbor in nextNeighbors:
                 edge = (destNode, neighbor)
                 if neighbor not in vertices:
-                    vertexQueue.put(edge)
-
+                    stack.append(currEdge)
+                    currEdge = edge
                     vertices.append(neighbor)
                     edges.append(edge)
+                    unVisitedNeighbor = True
+                    break
 
+            if unVisitedNeighbor == False:
+                currEdge = stack.pop()
 
     # This is for the last connected component.
     treeEdges.append(edges)
@@ -163,14 +170,54 @@ def findMinSpanTrees(adjacencyList):
 
     return (treeEdges, treesAdjList)
 
+# Ordering the vertices so that we can do the bridge algorithm well.
+def preOrderVertices(edges, numVertices):
+    # Since the edges are preordered, we just go through the edges.
+
+    # Just to ensure that we got all the vertices.
+    vertices = []
+
+    # Return value. This list will hold tuples; first index holding the 
+    # order of the vertex. Second index holding the id of the node.
+    orderedVertices = []
+
+    # Starting order:
+    orderIdx = 1
+
+    # Starting with the root node.
+    rootEdge = edges[0]
+    rootVert = rootEdge[0]
+
+    # Creating the tuple
+    orderedVertex = (orderIdx, rootVert)
+
+    # Adding it to the lists so we can keep track of the met vertices.
+    vertices.append(rootVert)
+    orderedVertices.append(orderedVertex)
+
+    for edge in edges:
+        continue
+
+
+
+
+
 # Using minimum spanning trees, we will be finding the bridge vertices.
 # This uses Tarjan's algorithm.
 def findBridges(adjacencyList):
+    # Getting the the total number of vertices:
+    numVertices = len(adjacencyList.keys())
+
     # First, we need to find the minimum spanning trees
     spanEdges, spanTrees = findMinSpanTrees(adjacencyList)
-    print adjacencyList
-    print spanTrees
+    listToJSON(spanTrees[0])
+    # Now, we iterate through the edges in pre-order, which
+    # is basically what it is right now. (thanks DFS :))
+    
 
+
+def ND(node, adjacencyList):
+    pass
 # This basically parses the stuff and then invokes the bridge.
 def run(rounds = 50):
     # This is the JSON file that will be parsed.
--- visualize.py
+++ visualize.py
@@ -2,15 +2,16 @@ import json
 from networkx.readwrite import json_graph
 import networkx as nx
 import matplotlib.pyplot as plt
+import sys
 
-filename = '2.5.2'
+filename = sys.argv[1]
 G = nx.Graph()
 with open(filename) as json_file:
     data = json.load(json_file)
-    for i in range(len(data)):
-        G.add_node(i)
-        for j in range(len(data[str(i)])):
-            G.add_edge(i, int(data[str(i)][j]))
+    for key in data.keys():
+        G.add_node(int(key))
+        for val in data[key]:
+            G.add_edge(int(key), int(val))
 dic = {}
 print G.size()
 for i in range(len(G.nodes())):
