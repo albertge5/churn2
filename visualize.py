9ef8b34c96f8c1d87dc39932b48b526e0b8a31cd
timothychou <tchou@caltech.edu>
2016-02-28 17:39:21 -0800

    merge

--- visualize.py
+++ visualize.py
@@ -1,9 +1,10 @@
 import json
+import sys
 from networkx.readwrite import json_graph
 import networkx as nx
 import matplotlib.pyplot as plt
 
-filename = '2.5.2'
+filename = sys.argv[1]
 G = nx.Graph()
 with open(filename) as json_file:
     data = json.load(json_file)
@@ -17,8 +18,8 @@ for i in range(len(G.nodes())):
     dic[i] = i
 for i in range(1):
     plt.figure()
-    plt.title('spring')
-    nx.draw_spring(G, node_size=0,width=.2, labels=dic, font_size=8, font_color='red')
+    plt.title(filename)
+    nx.draw_spring(G, node_size=0,width=.05, labels=dic, font_size=9, font_color='red')
 
 
 plt.show()
